7224cf38e1582dcef548318c76b0f1f29fca5fad
jacksonbmitchusson <mr080guy@gmail.com>
2025-11-02 02:51:18 -0600

    make data_collection async

--- data_collector.py
+++ data_collector.py
@@ -1,42 +1,51 @@
 from cv2 import VideoCapture, imwrite
 from datetime import datetime
 from pytz import timezone
-import time
+import asyncio
 import smbus2 # type: ignore
 import bme280 # type: ignore
 
-# DATA GATHER
 addr = 0x77
 bus = smbus2.SMBus(1)
 calib_params = bme280.load_calibration_params(bus, address=addr)
 cam = [VideoCapture(0), VideoCapture(2)]
 
 # seconds
-delay = 120
+image_delay = 180
+env_delay = 30
 output_path = '/home/onaquest/server-output'
 
-def capture_data():
-    while True:
-        print('logginnggg')
-        timestamp = datetime.now(timezone('America/Chicago')).strftime('%m-%d_%H-%M')
-        capture_temphumid(timestamp)
-        capture_image(timestamp, 0)
-        time.sleep(delay/2)
-        capture_image(timestamp, 1)
-        time.sleep(delay/2)
+def get_timestamp():
+    return datetime.now(timezone('America/Chicago')).strftime('%m-%d_%H-%M-%S')
 
-def capture_temphumid(timestamp):
-    data = bme280.sample(bus, addr)
-    t = (data.temperature * 9/5) + 32
-    h = data.humidity
-    p = data.pressure
-    record_str = f'{timestamp} - {t:05.2f}°F {h:05.2f}% {p:05.2f} hPa'
-
-    with open(f'{output_path}/environment_log.txt', 'a') as log:
-        log.write('\n' + record_str)
 
 def capture_image(timestamp, id):
+    print(f'Capturing image with camera {id}...')
     ret, frame = cam[id].read()
     if ret:
         path = f'{output_path}/images{id}/{timestamp}.png'
         imwrite(path, frame)
+
+async def capture_images():
+    while True:    
+        capture_image(get_timestamp(), 0)
+        asyncio.sleep(image_delay/2)
+        capture_image(get_timestamp(), 1)
+        asyncio.sleep(image_delay/2)
+
+async def capture_env():
+    while True:
+        print(f'Capturing env...')
+        data = bme280.sample(bus, addr)
+        t = (data.temperature * 9/5) + 32
+        h = data.humidity
+        p = data.pressure
+        record_str = f'{get_timestamp()} - {t:05.2f}°F {h:05.2f}% {p:05.2f} hPa'
+
+        with open(f'{output_path}/environment_log.txt', 'a') as log:
+            log.write('\n' + record_str)
+
+        asyncio.sleep(env_delay)
+        
+
+    

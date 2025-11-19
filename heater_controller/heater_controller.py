8767f0f0d7d62a9b71d181b981e7a462bfb82cb4
jacksonbmitchusson <mr080guy@gmail.com>
2025-11-19 00:55:20 -0600

    heater controller

---
+++ heater_controller/heater_controller.py
@@ -0,0 +1,58 @@
+import requests
+from util.envparse import last_record, parse_record
+from matplotlib import pyplot as plt
+from time import sleep
+from datetime import datetime, timedelta, timezone
+
+env_path = '/home/onaquest/server-output/environment_log.txt'
+
+def get_timestamp():
+    return datetime.now(timezone('America/Chicago')).strftime('%m-%d_%H-%M-%S')
+
+def duty_curve(temp_delta, params): 
+    min_th_diff, max_th_diff, min_ontime, max_ontime = params
+    if temp_delta < min_th_diff:
+        return 0
+    if temp_delta > max_th_diff:
+        return max_ontime
+    lerp_amt = (temp_delta - min_th_diff)/(max_th_diff - min_th_diff)
+    return min_ontime*(1 - lerp_amt) + max_ontime*lerp_amt
+detail = 100
+
+def turn_heater(state):
+    requests.get(f'http://192.168.1.246/relay/0?turn={state}')
+
+def debug_show_curve(params):
+    x = [xi/detail for xi in range(-2*detail, 7*detail)]
+    y = [duty_curve(xi, params) for xi in x]
+    plt.plot(x, y)
+    plt.show()
+
+def append_log(temp_delta, ontime, cycle_seconds):
+    # time, temp_delta, ontime %, ontime seconds
+    timestamp = get_timestamp()
+    with open(f'./heater_log.txt', 'a') as log:
+        log.write(f'{timestamp} - temp_delta: {temp_delta} deg F, ontime %: {(100*ontime):.2f}%, ontime seconds: {ontime*cycle_seconds}\n')
+
+def duty_cycle(params, cycle_time: timedelta):
+    record = parse_record(last_record(env_path))
+    record_timedelta = record['date'] - datetime.now(timezone('America/Chicago'))
+    if record_timedelta < cycle_time:    
+        temp_delta = target_temp - record['Temperature']
+        ontime = duty_curve(temp_delta, params)
+        cycle_seconds = cycle_time.total_seconds()
+        append_log(temp_delta, ontime, cycle_seconds)
+        turn_heater('on')
+        sleep(ontime*cycle_seconds)
+        turn_heater('off')
+        sleep((1 - ontime)*cycle_seconds)
+    else: 
+        sleep(cycle_time)
+
+
+params = (-1, 5, .1, .9)
+debug_show_curve(params)
+duty_cycle(params)
+
+target_temp = 75
+cycle_time = timedelta(seconds=180)

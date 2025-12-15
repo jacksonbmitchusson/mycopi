402a2d29a60d91048429fc25eea30ff0626d94f8
jacksonbmitchusson <mr080guy@gmail.com>
2025-12-15 01:06:30 -0600

    final fix i swear

--- util/envparse.py
+++ util/envparse.py
@@ -11,7 +11,7 @@ def last_record(envpath):
         return f.readline().decode()
     
 def parse_record(raw: str) -> dict:
-    tokens = findall(r'([0-9]+\.[0-9]{2}|[0-9]{2})|[0-9]{4}', raw)
+    tokens = findall(r'([0-9]{4}|[0-9]+\.[0-9]{2}|[0-9]{2})', raw)
     M, d, y, h, m, s = map(int, tokens[:6])
     date = datetime(y, M, d, h, m, s, tzinfo=timezone('America/Chicago'))
     temp, humidity, pressure = map(float, tokens[6:])

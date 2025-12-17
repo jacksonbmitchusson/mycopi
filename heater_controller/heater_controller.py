5984b7f806d927156038301177af6f41e1e19ee5
jacksonbmitchusson <mr080guy@gmail.com>
2025-12-16 22:23:34 -0600

    temp adjust

--- heater_controller/heater_controller.py
+++ heater_controller/heater_controller.py
@@ -53,7 +53,7 @@ def duty_cycle(params, target_temp, cycle_time: timedelta):
 
 
 params = (-1, 0.05, 2.5, 0.7)
-target_temp = 75
+target_temp = 73.5
 cycle_time = timedelta(seconds=180)
 
 while True:

5a4c7b4ba72a4516ed5bbf708762a514ad468e4a
jacksonbmitchusson <mr080guy@gmail.com>
2026-07-08 04:50:05 -0500

    faster faster

--- auto_timelapse/timelapser.py
+++ auto_timelapse/timelapser.py
@@ -59,8 +59,8 @@ command_ls = ['ffmpeg',
               '-i', f'{output_path}/filelist.txt', 
               '-pix_fmt', 'yuv420p', 
               '-c:v', 'libx264', 
-              '-crf', '18', 
-              '-preset', 'medium', 
+              '-crf', '22', 
+              '-preset', 'veryfast', 
               '-r', str(framerate), 
               f'{output_path}/output.mp4']
 subprocess.run(command_ls)
